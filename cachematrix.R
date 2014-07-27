5fce02ef757fe900ba05ee3e581ae5170860af1f
NewMountain <christopher.nyberg@gmail.com>
2014-07-26 23:24:13 -0700

    Update cachematrix.R
    Added comments and made it a touch cleaner

--- cachematrix.R
+++ cachematrix.R
@@ -8,30 +8,31 @@
 
 
 ##Cache the matrix when handed an argument
-makeCacheMatrix <- function(x = matrix()) {
-    m <- NULL
+makeCacheMatrix <- function(x = matrix()) { ##Accept argument here
+    m <- NULL ## Define local variables here
     set <- function(y) {
-        x <<- y
+        x <<- y ## Set Public variables here
         m <<- NULL
     }
     get <- function() x
     setmatrix <- function(mean) m <<- mean
     getmatrix <- function() m
-    list(set = set, get = get,
+    list(set = set, get = get, ## Set list names and elements here
         setmatrix = setmatrix,
         getmatrix = getmatrix)
 }
 
 ## Calculate or return cached inverse of matrix - labelled as getmatrix and setmatrix
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...) { ##Accept argument here
         ## Return a matrix that is the inverse of 'x'
     m <- x$getmatrix()
-    if(!is.null(m)) {
-        message("getting cached data")
+    if(!is.null(m)) {  ## Test if the data has already been cached - if yes, inform user and return cached data
+        message("Data has been cached already - returning now")
         return(m)
     }
     data <- x$get()
-    m <- solve(data, ...)
+    m <- solve(data, ...)  ##If the inverse matrix has not been cached, perform the calculation here and cache
     x$setmatrix(m)
-    m
+    message("Data has not yet been cached - calculating and cache-ing now")
+    m  ##return "fresh" value
 }
